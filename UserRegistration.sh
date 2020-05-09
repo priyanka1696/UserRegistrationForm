bfb9821d83cfb18c83c4801359cf804fdcf0b1d3
Priyanka <fulmogarepriya@gmail.com>
2020-05-10 00:07:31 +0530

    Added the User EmailId

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -4,7 +4,7 @@ echo "Welcome to User Registration Form"
 echo "Enter User First Name :"
 read firstName;
 firstNamepattern="^[A-Z][a-z]*$"
-if [[ $firstName =~ $Namepattern ]]
+if [[ $firstName =~ $firstNamepattern ]]
 then
     echo "Valid"
 else
@@ -14,10 +14,20 @@ fi
 echo "Enter User last Name :"
 read lastName;
 lastNamepattern="^[A-Z][a-z]*$"
-if [[ $lastName =~ $Namepattern ]]
+if [[ $lastName =~ $lastNamepattern ]]
 then
     echo "Valid"
 else
     echo "Invalid"
+fi
+
+echo "Enter the User EmailId:"
+read EmailId;
+EmailIdpattern="^[a-z A-Z 0-9 . - _ +]*[@][a-z 0-9]*[.][a-z ,]*[.][a-z ,]*$"
+if [[ $EmailId =~ $EmailIdpattern ]]
+then
+        echo "valid EmailId"
+else
+        echo "Invalid EmailId"
 fi
 
