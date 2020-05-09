0d6ffc31ec736e60831c4027ed53366d6719c266
Priyanka <fulmogarepriya@gmail.com>
2020-05-09 23:12:11 +0530

    Added the User First Name

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,2 +1,12 @@
 #!/bin/bash -x
 echo "Welcome to User Registration Form"
+
+echo "Enter User First Name :"
+read firstName;
+firstNamepattern="^[A-Z][a-z]*$"
+if [[ $firstName =~ $Namepattern ]]
+then
+    echo "Valid"
+else
+    echo "Invalid"
+fi
